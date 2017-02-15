a7aebd898ce2b28059bdb2e4436ffa386fad4330
王志刚 <wang.zhigang@wangzhigangdeMacBook-Pro.local>
2017-02-15 14:42:57 +0800

    check npm or yarn build script

--- exec.sh
+++ exec.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
+export CONFIG=${CONFIG:-"npm"}
+export ARG=$2
+
+if [[ "$1" == "yarn" ]]; then
+  CONFIG="yarn"
+fi
+
 echo "-------work dir----------"
 echo $PWD
 echo "--------install----------"
-yarn install
+if [ "$CONFIG" == "npm" ] ; then
+  npm install
+else
+  yarn install
+fi
 echo "---------build-----------"
-yarn run build
+if [ "$CONFIG" == "npm" ] ; then
+  npm run $ARG
+else
+  yarn run $ARG
+fi
 exit 0;
 
